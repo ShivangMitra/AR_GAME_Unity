1cf48307bf6c2eadd9e7de5d5e7bc1c1ce8e26a6
Shivang Mitra <shivangmitra8@gmail.com>
2022-03-26 23:02:32 +0530

    Completed AR Game Minor Project

---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+
+    public GameObject arCamera;
+
+    public AudioSource source;
+    public AudioClip clip;
+
+    public int kills = 0;
+    public void shoot(){
+        RaycastHit hit;
+
+        if(Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit)){
+            kills += 1;
+            source.PlayOneShot(clip);
+            Destroy(hit.transform.gameObject);
+        }
+
+        if(kills == 9){
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
